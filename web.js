73334f48aa8d9a5bd01ae210e5a908bd31d181b0
E6600ub <e6600ub@UbuntuVM.(none)>
2013-07-15 16:59:18 +0200

    Port auf 8080 geaendert

--- web.js
+++ web.js
@@ -8,7 +8,9 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html", "utf-8"));
 });
 
-var port = process.env.PORT || 5000;
+// Port von 5000 auf 8080 geaendert. Auch in AWS frei gegeben.
+// Aufruf ueber node web.js oder Browser.
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
