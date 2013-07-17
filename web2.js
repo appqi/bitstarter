e11af7be6cec8ef49d870b21f844c024798f437f
Mathias Hentrich <appqi@gmx.net>
2013-07-17 11:08:51 +0200

    responsives Design und assets Ordner hinzugefügt

--- web2.js
+++ web2.js
@@ -9,6 +9,11 @@ app.get('/', function(request, response) {
 });
 
 
+app.use(express.favicon());
+app.use(express.logger());
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/assets/css', express.static(__dirname + '/assets/css'));
+app.use('/assets/js', express.static(__dirname + '/assets/js'));
 
 // Port von 5000 auf 8080 geaendert. Auch in AWS frei gegeben.
 // Aufruf ueber node web.js oder Browser.
